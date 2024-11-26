e15b90310d9806d2a3842875179a88e3d144d006
Bob Richter <mail@bobrichter.de>
2024-11-27 00:48:39 +0100

    Implement angular and lateral perception

--- scripts/ObstacleAvoidanceNode.py
+++ scripts/ObstacleAvoidanceNode.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 
 import rospy
+import itertools
 import numpy as np
 
 from RuleNode import RuleNode
 from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3, Pose
 from reynolds_rules.msg import VectorArray
 from typing import List, Tuple
 
 
-Position = Tuple[float, float]
-Vector = Tuple[float, float]
+Position2D = Tuple[float, float]
 
-
-def to_vector3(v: Vector) -> Vector3:
-    result = Vector3()
-    result.x = v[0]
-    result.y = v[1]
-    return result
+ANGULAR_STEPSIZE = np.deg2rad(2)
 
 
 class ObstacleAvoidanceNode(RuleNode):
     def __init__(self):
         super().__init__("obstacle_avoidance", 10)
 
+        # maximum range of perception
         self.view_range = rospy.get_param("~view_range", 0.5)
-        self.view_angle = rospy.get_param("~view_angle", 90)
+        # view_angle [degrees]: angular range of perception (half-width of the perception cone)
+        self.view_angle = rospy.get_param("~view_angle", 60)
+        # view_split [degrees]: angle that splits the perception cone on each side
+        self.view_split = rospy.get_param("~view_split", 0)
+        # example: view_angle:=60 view_split:=15
+        #   -> angular ranges [-75:-15] and [15:75] are observed
+
+        print("Starting the ObstacleAvoidance node.")
+        print(f"view_range: {self.view_range}")
+        print(f"view_angle: {self.view_angle : >3} deg")
+        print(f"view_split: {self.view_split : >3} deg")
 
         self.map: OccupancyGrid | None = None
         rospy.Subscriber("map", OccupancyGrid, self.callback_map)
@@ -36,13 +42,80 @@ class ObstacleAvoidanceNode(RuleNode):
 
     # implement abstract method
     def control_cycle(self, _):
-        obstacle_avoidance_vectors = self.obstacle_avoidance_rule()
-
         data = VectorArray()
-        data.vectors = [to_vector3(v) for v in obstacle_avoidance_vectors]
+        data.vectors = [
+            self.sum_weighted_repellent_vectors(i) for i in range(self.n_robots)
+        ]
         self.pub.publish(data)
 
-    def map_lookup(self, pos: Position) -> bool:
+    def sum_weighted_repellent_vectors(self, robot_index: int) -> Vector3:
+        """
+        Find obstacles in the view range and compute the weighted sum of the distance vectors.
+        The weight for each distance vector is (-1 / distance) to simulate a repellent force.
+        """
+        if not self.robots[robot_index]:
+            return Vector3()
+
+        robot_pose = self.robots[robot_index].pose.pose
+        obstacles = self.find_obstacles(robot_pose)
+        if len(obstacles) == 0:
+            return Vector3()
+
+        robot_position = np.array((robot_pose.position.x, robot_pose.position.y))
+
+        v = np.array((0.0, 0.0))
+        for obstacle in obstacles:
+            opposite_vector = robot_position - np.array(obstacle)
+            distance = np.linalg.norm(opposite_vector)
+            if distance != 0:
+                v += opposite_vector / distance
+
+        v = v / len(obstacles)
+
+        v3 = Vector3()
+        v3.x = v[0]
+        v3.y = v[1]
+        return v3
+
+    def find_obstacles(self, robot_pose: Pose) -> List[Position2D]:
+        """
+        Return the position of obstacles around the robot_position
+        Current implementation: square with size (2*self.view_range)^2
+        """
+        if not self.map:
+            return []
+
+        robot_x = robot_pose.position.x
+        robot_y = robot_pose.position.y
+        robot_q = robot_pose.orientation
+
+        robot_yaw = 2 * np.arctan2(robot_q.z, robot_q.w)
+
+        # look left and right
+        min_angle = np.deg2rad(self.view_split)
+        max_angle = np.deg2rad(self.view_split + self.view_angle)
+        s = ANGULAR_STEPSIZE
+        angular_range = itertools.chain(
+            range(int(-max_angle / s), int(-min_angle / s)),  # left
+            range(int(min_angle / s), int(max_angle / s)),  # right
+        )
+
+        radial_step_size = self.map.info.resolution
+        n_radial = int(self.view_range / radial_step_size)
+
+        obstacles = []
+        for a in angular_range:
+            alpha = robot_yaw + a * ANGULAR_STEPSIZE
+            cos_alpha = np.cos(alpha)
+            sin_alpha = np.sin(alpha)
+            for r in range(1, n_radial):
+                distance = r * radial_step_size
+                p = (robot_x + distance * cos_alpha, robot_y + distance * sin_alpha)
+                if self.map_lookup(p):
+                    obstacles.append(p)
+        return obstacles
+
+    def map_lookup(self, pos: Position2D) -> bool:
         """
         Return True if the position is within the map and there is an obstacle, else False
         """
@@ -54,55 +127,6 @@ class ObstacleAvoidanceNode(RuleNode):
         if not 0 <= index < len(self.map.data):
             return False
         return self.map.data[index] == 100
-
-    def find_obstacles(self, robot_position: Position) -> List[Position]:
-        """
-        Return the position of obstacles around the robot_position
-        Current implementation: square with size (2*self.view_range)^2
-        """
-        if not self.map:
-            return []
-
-        step_size = self.map.info.resolution
-        n = int(self.view_range / step_size)
-
-        obstacles: List[Position] = []
-        for i in range(-n, n + 1):
-            for j in range(-n, n + 1):
-                pos = (
-                    robot_position[0] + j * step_size,
-                    robot_position[1] + i * step_size,
-                )
-                if self.map_lookup(pos):
-                    obstacles.append(pos)
-        return obstacles
-
-    def sum_weighted_repellent_vectors(
-        self, robot_position: Position, obstacles: List[Position]
-    ) -> Vector:
-        """
-        Calculate a repellent force from each obstacle and return the sum of all of those vectors
-        """
-        vec = (0, 0)
-        for obstacle in obstacles:
-            opposite_vector = np.array(robot_position) - np.array(obstacle)
-            distance = np.linalg.norm(opposite_vector)
-            if distance != 0:
-                vec += opposite_vector / distance
-        return vec
-
-    def obstacle_avoidance_rule(self) -> List[Vector]:
-        """
-        Compute the sum of repellent vectors for each robot
-        """
-        vectors: list[Vector] = [(0, 0)] * self.n_robots
-        for i in range(self.n_robots):
-            robot = self.robots[i]
-            if robot:
-                pos = (robot.pose.pose.position.x, robot.pose.pose.position.y)
-                obstacles = self.find_obstacles(pos)
-                vectors[i] = self.sum_weighted_repellent_vectors(pos, obstacles)
-        return vectors
 
 
 if __name__ == "__main__":
