68348230d7369e2669cf7b7fea622c68ebe44963
Bob Richter <mail@bobrichter.de>
2024-11-26 15:52:04 +0100

    Base structure for ReynoldsRulesNode

---
+++ reynolds_rules/scripts/ReynoldsRulesNode.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import rospy
+
+
+class ReynoldsRulesNode:
+    def __init__(self):
+        pass
+
+    def map_subscription(self):
+        pass
+
+    def map_lookup(self):
+        pass
+
+    def separation_rule(self):
+        pass
+
+    def alignment_rule(self):
+        pass
+
+    def cohesion_rule(self):
+        pass
+
+    def navigation_rule(self):
+        pass
+
+    def navigation_rule(self):
+        pass
+
+    def obstacle_avoidance_rule(self):
+        pass
+
+    def weighted_sum(self):
+        pass
+
+
+if __name__ == "__main__":
+    rospy.init_node("ReynoldsRulesNode")
+    try:
+        node = SeparationNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
