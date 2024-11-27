5d8667badd74a11811b27abb8a94cb99f2d1dd67
Bob Richter <mail@bobrichter.de>
2024-11-27 14:43:42 +0100

    Make RuleNode python 3.8 compatible

--- scripts/RuleNode.py
+++ scripts/RuleNode.py
@@ -3,18 +3,19 @@
 import rospy
 
 from nav_msgs.msg import Odometry
-from reynolds_rules.msg import ArrayVectors # Import the custom message
+from reynolds_rules.msg import ArrayVectors
+
 
 # Parent class os rules nodes classes
 class RuleNode:
-    def __init__(self, name, rate):
+    def __init__(self, name: str, rate_per_second: int = 10):
         self.n_robots = rospy.get_param("~number_robots", 10)
         self.multiplier = rospy.get_param("~" + name + "_multiplier", 1)
 
         # Set common atributes of rules
         self.robots = [None] * self.n_robots
 
-        rospy.Timer(rospy.Duration(1 / rate), self.control_cycle)
+        rospy.Timer(rospy.Duration(1 / rate_per_second), self.control_cycle)
         self.pub = rospy.Publisher("/" + name + "_vectors", ArrayVectors, queue_size=1)
 
         # Creates a suscriber for each robot, but with the same callback
@@ -23,9 +24,11 @@ class RuleNode:
             rospy.Subscriber(topic, Odometry, self.robot_callback)
 
     # Saves and updates list with odom of all robots
-    def robot_callback(self, data):
-        id = int(data.header.frame_id.removesuffix("/odom").removeprefix("/robot_"))
-        self.robots[id] = data
+    def robot_callback(self, data: Odometry):
+        begin = data.header.frame_id.find("/robot_") + 7
+        end = data.header.frame_id.find("/odom")
+        i = int(data.header.frame_id[begin:end])
+        self.robots[i] = data
 
     # Abstract method
     def control_cycle(self, _):
