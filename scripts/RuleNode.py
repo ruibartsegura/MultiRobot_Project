40666c26386080b5efb3e72b50744d22fee05ed8
jagutic <ja.gutierrezc.2022@alumnos.urjc.es>
2024-12-02 14:06:18 +0100

    Add launcher, remove extra folder, improve RuleNode

--- scripts/RuleNode.py
+++ scripts/RuleNode.py
@@ -9,16 +9,19 @@ from reynolds_rules.msg import VectorArray
 # Parent class os rules nodes classes
 class RuleNode:
     def __init__(self, name, rate):
+        print("Activating" + name + "_rule node...")
+
         self.n_robots = rospy.get_param("~number_robots", 10)
         self.multiplier = rospy.get_param("~" + name + "_multiplier", 1)
 
-        # Set common atributes of rules
+        # Set common atributes of rules (Robots odom list, timer, publisher)
         self.robots = []
 
         rospy.Timer(rospy.Duration(1 / rate), self.control_cycle)
         self.pub = rospy.Publisher("/" + name + "_vectors", VectorArray, queue_size=1)
 
-        # Creates a suscriber for each robot, but with the same callback
+        # Creates a suscriber for each robot odometry,
+        # but using the same callback
         for i in range(self.n_robots):
             topic = "/robot_" + str(i) + "/odom"
             rospy.Subscriber(topic, Odometry, self.robot_callback)
@@ -33,6 +36,6 @@ class RuleNode:
         i = int(data.header.frame_id[begin:end])
         self.robots[i] = data
 
-    # Abstract. Principal method.
+    # Abstract, principal method.
     def control_cycle(self, _):
         pass
