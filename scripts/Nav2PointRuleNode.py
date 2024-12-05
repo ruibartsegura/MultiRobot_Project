88887c444a9b1d326b3fe34531f1894a50fdf2a5
Bob Richter <mail@bobrichter.de>
2024-12-05 20:39:42 +0100

    Finished path finding in nav2point

--- scripts/Nav2PointRuleNode.py
+++ scripts/Nav2PointRuleNode.py
@@ -12,6 +12,12 @@ from reynolds_rules.msg import VectorArray
 from navigation.GridMap import GridMap
 
 
+def distance(p1: Point, p2: Point) -> float:
+    x = p2.x - p1.x
+    y = p2.y - p1.y
+    return math.sqrt(x * x + y * y)
+
+
 class Nav2PointRuleNode(RuleNode):
     def __init__(self):
         super().__init__("nav2point")
@@ -21,10 +27,12 @@ class Nav2PointRuleNode(RuleNode):
         self.point.y = rospy.get_param("~point_y", 0)
         self.threshold_vel = rospy.get_param("~threshold_vel", 2)
         self.grid_resolution = rospy.get_param("~grid_resolution", 10)
+        self.distance_to_waypoint = rospy.get_param("~distance_to_waypoint", 1.0)
 
         print(f"  point_x: {self.point.x}")
         print(f"  point_y: {self.point.y}")
 
+        self.paths = [[]] * self.n_robots
         self.map: OccupancyGrid | None = None
         rospy.Subscriber("map", OccupancyGrid, self.callback_map)
 
@@ -33,6 +41,15 @@ class Nav2PointRuleNode(RuleNode):
     def callback_map(self, msg: OccupancyGrid):
         self.map = msg
         self.grid = GridMap(msg, self.grid_resolution)
+        self.compute_paths()
+
+    def compute_paths(self):
+        if not self.map:
+            return
+
+        positions = [robot.pose.pose.position for robot in self.robots]
+        goal = self.point
+        self.paths = [self.grid.a_star(pos, goal) for pos in positions]
 
     # Return vector from point 1 to 2
     # Limits vector to a threshold
@@ -42,27 +59,41 @@ class Nav2PointRuleNode(RuleNode):
         vector.x = point2.x - point1.x
         vector.y = point2.y - point1.y
 
-        vector_lenght = math.sqrt(vector.x * vector.x + vector.y * vector.y)
-        if vector_lenght > self.threshold_vel:
-            factor = self.threshold_vel / vector_lenght
+        vector_length = math.sqrt(vector.x * vector.x + vector.y * vector.y)
+        if vector_length > self.threshold_vel:
+            factor = self.threshold_vel / vector_length
             vector.x *= factor
             vector.y *= factor
 
         return vector
 
+    def vector_to_next_waypoint(self, robot_index) -> Vector3:
+        if len(self.paths[robot_index]) == 0:
+            return Vector3()
+
+        robot_position = self.robots[robot_index].pose.pose.position
+
+        # if the robot reached the waypoint, remove the waypoint unless it is the final goal
+        if len(self.paths[robot_index]) != 1 and (
+            distance(robot_position, self.paths[robot_index][0])
+            < self.distance_to_waypoint
+        ):
+            self.paths[robot_index] = self.paths[robot_index][1:]
+
+        return self.calc_vector(robot_position, self.paths[robot_index][0])
+
     # Make and publish array of velocity vector to given point
     def control_cycle(self, _):
         # Check if there is a new target point
-        self.point.x = rospy.get_param("~point_x", self.point.x)
-        self.point.y = rospy.get_param("~point_y", self.point.y)
+        x = rospy.get_param("~point_x", self.point.x)
+        y = rospy.get_param("~point_y", self.point.y)
+        if x != self.point.x or y != self.point.y:
+            self.point.x = x
+            self.point.y = y
+            self.compute_paths()
 
-        nav2point_vectors = VectorArray()
-
-        for robot in self.robots:
-            nav2point_vector = self.calc_vector(robot.pose.pose.position, self.point)
-            nav2point_vectors.vectors.append(nav2point_vector)
-
-        self.pub.publish(nav2point_vectors)
+        vectors = [self.vector_to_next_waypoint(i) for i in range(self.n_robots)]
+        self.pub.publish(VectorArray(vectors=vectors))
 
 
 if __name__ == "__main__":
--- scripts/navigation/GridMap.py
+++ scripts/navigation/GridMap.py
@@ -2,23 +2,28 @@
 
 import math
 import numpy as np
+import heapq
 
-from RuleNode import RuleNode
-from geometry_msgs.msg import Point, Vector3
+from geometry_msgs.msg import Point
 from nav_msgs.msg import OccupancyGrid
-from typing import Tuple
-
-from navigation.AStar import a_star
+from typing import Tuple, List
 
 
-def is_occupied(map: OccupancyGrid, i, j) -> bool:
+def distance(point1, point2):
+    x1, y1 = point1[0:2]
+    x2, y2 = point2[0:2]
+    dist2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+    return math.sqrt(dist2)
+
+
+def is_occupied(map: OccupancyGrid, i: int, j: int) -> bool:
     index = map.info.width * i + j
     if not 0 <= index < len(map.data):
         return False
     return map.data[index] == 100
 
 
-def is_gridcell_occupied(map: OccupancyGrid, cellsize: int, i, j) -> bool:
+def is_gridcell_occupied(map: OccupancyGrid, cellsize: int, i: int, j: int) -> bool:
     i_range = range(i * cellsize, (i + 1) * cellsize)
     j_range = range(j * cellsize, (j + 1) * cellsize)
     for i in i_range:
@@ -29,10 +34,12 @@ def is_gridcell_occupied(map: OccupancyGrid, cellsize: int, i, j) -> bool:
 
 
 class GridMap:
-    def __init__(self, map: OccupancyGrid, cellsize: int):
+    def __init__(self, map: OccupancyGrid, cellsize: int, movement="8N"):
+        """param movement can be either 4-connectivity ('4N') or 8-connectivity ('8N', default)"""
         self.resolution = map.info.resolution * cellsize
         self.origin = map.info.origin.position
 
+        # generate low resolution grid map for
         height = int(map.info.height / cellsize) + 1
         width = int(map.info.width / cellsize) + 1
         self.grid = np.ndarray((height, width), dtype="bool")
@@ -40,15 +47,25 @@ class GridMap:
             for j in range(width):
                 self.grid[i][j] = is_gridcell_occupied(map, cellsize, i, j)
 
-        path, path_idx = a_star((1, 0), (4, 3), self, occupancy_cost_factor=10)
-        for i in range(len(path_idx)):
-            print(f"{path[i][0] : >+2.1f}, {path[i][1]: >+2.1f}  ({path_idx[i]})  ({self.is_occupied_idx(path_idx[i])})")
+        # initialize the possible movements for the path-finding algorithm
+        if movement == "4N":
+            self.movements = [(1, 0, 1.0), (0, 1, 1.0), (-1, 0, 1.0), (0, -1, 1.0)]
+        elif movement == "8N":
+            self.movements = [
+                (1, 0, 1.0),
+                (0, 1, 1.0),
+                (-1, 0, 1.0),
+                (0, -1, 1.0),
+                (1, 1, math.sqrt(2)),
+                (-1, 1, math.sqrt(2)),
+                (-1, -1, math.sqrt(2)),
+                (1, -1, math.sqrt(2)),
+            ]
+        else:
+            raise ValueError("Unknown movement")
 
-    def shape(self) -> Tuple[int, int]:
-        return self.grid.shape
-
-    def is_occupied_xy(self, x, y) -> bool:
-        (i, j) = self.get_index_from_coordinates(x, y)
+    def is_occupied_xy(self, xy: Tuple[int, int]) -> bool:
+        (i, j) = self.world2grid(xy)
         return self.grid[i][j]
 
     def is_occupied_idx(self, ij: Tuple[int, int]) -> bool:
@@ -57,13 +74,93 @@ class GridMap:
     def is_inside_idx(self, ij: Tuple[int, int]) -> bool:
         return 0 <= ij[0] < self.grid.shape[0] and 0 <= ij[1] < self.grid.shape[1]
 
-    def get_index_from_coordinates(self, x, y) -> Tuple[int, int]:
-        i = int((y - self.origin.y) / self.resolution)
-        j = int((x - self.origin.x) / self.resolution)
+    def world2grid(self, xy: Tuple[int, int]) -> Tuple[int, int]:
+        """Returns the grid indices for given x and y coordinates."""
+        i = int((xy[1] - self.origin.y) / self.resolution)
+        j = int((xy[0] - self.origin.x) / self.resolution)
         return (i, j)
 
-    def get_coordinates_from_index(self, ij: Tuple[int, int]):
-        i, j = ij
-        x = j * self.resolution + self.origin.x
-        y = i * self.resolution + self.origin.y
+    def grid2world(self, ij: Tuple[int, int]) -> Tuple[int, int]:
+        """Returns x and y coordinates for given grid indices."""
+        x = ij[1] * self.resolution + self.origin.x + self.resolution / 2
+        y = ij[0] * self.resolution + self.origin.y + self.resolution / 2
         return (x, y)
+
+    def a_star(self, start_xy: Point, goal_xy: Point) -> List[Point]:
+        """A* algorithm. Returns the resulting path as a list of waypoints."""
+        # get array indices of start and goal
+        start = self.world2grid((start_xy.x, start_xy.y))
+        goal = self.world2grid((goal_xy.x, goal_xy.y))
+
+        visited = np.zeros(self.grid.shape, dtype="bool")
+
+        # check if start and goal nodes correspond to free spaces
+        if self.is_occupied_idx(start):
+            raise Exception("Start node is not traversable")
+
+        if self.is_occupied_idx(goal):
+            raise Exception("Goal node is not traversable")
+
+        # add start node to front
+        # front is a list of (total estimated cost to goal, total cost from start to node, node, previous node)
+        start_node_cost = 0
+        start_node_estimated_cost_to_goal = distance(start, goal) + start_node_cost
+        front = [(start_node_estimated_cost_to_goal, start_node_cost, start, None)]
+
+        # use a dictionary to remember where we came from in order to reconstruct the path later on
+        came_from = {}
+
+        # while there are elements to investigate in our front.
+        while front:
+            # get smallest item and remove from front.
+            element = heapq.heappop(front)
+
+            # if this has been visited already, skip it
+            _total_cost, cost, pos_idx, previous = element
+            if visited[pos_idx]:
+                continue
+
+            # now it has been visited, mark with cost
+            visited[pos_idx] = True
+
+            # set its previous node
+            came_from[pos_idx] = previous
+
+            # if the goal has been reached, we are done!
+            if pos_idx == goal:
+                break
+
+            # check all neighbors
+            for dx, dy, deltacost in self.movements:
+                # determine new position
+                new_x = pos_idx[0] + dx
+                new_y = pos_idx[1] + dy
+                new_pos = (new_x, new_y)
+
+                # check whether new position is inside the map
+                # if not, skip node
+                if not self.is_inside_idx(new_pos):
+                    continue
+
+                # add node to front if it was not visited before and is not an obstacle
+                if (not visited[new_pos]) and (not self.is_occupied_idx(new_pos)):
+                    cost = 10.0
+                    new_cost = cost + deltacost
+                    new_total_cost = new_cost + distance(new_pos, goal)
+
+                    heapq.heappush(front, (new_total_cost, new_cost, new_pos, pos_idx))
+
+        # reconstruct path backwards (only if we reached the goal)
+        path = []
+        if pos_idx == goal:
+            while pos_idx:
+                # convert to world coordinates
+                x, y = self.grid2world(pos_idx)
+                path.append(Point(x=x, y=y))
+                pos_idx = came_from[pos_idx]
+
+            # reverse so that path is from start to goal.
+            path.reverse()
+            path.append(goal_xy)
+
+        return path
