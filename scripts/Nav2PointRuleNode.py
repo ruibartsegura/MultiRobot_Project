ca4b298765555c9d5858dca2bf509206ecbd9c29
jagutic <ja.gutierrezc.2022@alumnos.urjc.es>
2024-12-05 23:04:55 +0100

    Fix doubled methods, extra import, code warnings

--- scripts/Nav2PointRuleNode.py
+++ scripts/Nav2PointRuleNode.py
@@ -8,10 +8,10 @@ from geometry_msgs.msg import Point
 from nav_msgs.msg import OccupancyGrid
 from typing import List, Tuple
 from reynolds_rules.msg import VectorArray
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Vector3
 
 Position2D = Tuple[float, float]
+
 
 # Return distance between two points
 def calc_distance(pos1, pos2):
@@ -74,11 +74,14 @@ class Nav2PointRuleNode(RuleNode):
                 else:
                     rospy.logwarn(f"Obstacle between {start} and {end}.")
 
-    def find_neighbors(self, waypoints: List[Point], current_wp: Point, step=2) -> List[Point]:
+    def find_neighbors(
+        self, waypoints: List[Point], current_wp: Point, step=2
+    ) -> List[Point]:
         neighbors = []
         for wp in waypoints:
-            if (abs(wp.x - current_wp.x) == step and wp.y == current_wp.y) or \
-               (abs(wp.y - current_wp.y) == step and wp.x == current_wp.x):
+            if (abs(wp.x - current_wp.x) == step and wp.y == current_wp.y) or (
+                abs(wp.y - current_wp.y) == step and wp.x == current_wp.x
+            ):
                 neighbors.append(wp)
         return neighbors
 
@@ -86,10 +89,18 @@ class Nav2PointRuleNode(RuleNode):
         if not self.map:
             return False
 
-        start_i = int((start[1] - self.map.info.origin.position.y) / self.map.info.resolution)
-        start_j = int((start[0] - self.map.info.origin.position.x) / self.map.info.resolution)
-        end_i = int((end[1] - self.map.info.origin.position.y) / self.map.info.resolution)
-        end_j = int((end[0] - self.map.info.origin.position.x) / self.map.info.resolution)
+        start_i = int(
+            (start[1] - self.map.info.origin.position.y) / self.map.info.resolution
+        )
+        start_j = int(
+            (start[0] - self.map.info.origin.position.x) / self.map.info.resolution
+        )
+        end_i = int(
+            (end[1] - self.map.info.origin.position.y) / self.map.info.resolution
+        )
+        end_j = int(
+            (end[0] - self.map.info.origin.position.x) / self.map.info.resolution
+        )
 
         delta_i = abs(end_i - start_i)
         delta_j = abs(end_j - start_j)
@@ -126,7 +137,7 @@ class Nav2PointRuleNode(RuleNode):
         """
         start_pos = (start.x, start.y)
         target_pos = (target.x, target.y)
-        
+
         queue = [(start_pos, [start_pos])]
         visited = set()
         visited.add(start_pos)
@@ -146,57 +157,9 @@ class Nav2PointRuleNode(RuleNode):
             # Check posible paths with the neighbors
             for neighbor in neighbors:
                 neighbor_pos = (neighbor.x, neighbor.y)
-                if neighbor_pos not in visited and self.is_path_clear(current_pos, neighbor_pos):
-                    visited.add(neighbor_pos)
-                    queue.append((neighbor_pos, path + [neighbor_pos]))
-
-        rospy.logwarn("No available route found..")
-        return []
-
-    # Return vector from point 1 to 2
-    # Limits vector to a threshold
-    def calc_vector(self, point1, point2):
-        vector = Vector3()
-
-        vector.x = point2.x - point1.x
-        vector.y = point2.y - point1.y
-
-        vector_lenght = math.sqrt(vector.x * vector.x + vector.y * vector.y)
-        if vector_lenght > self.threshold_vel:
-            factor = self.threshold_vel / vector_lenght
-            vector.x *= factor
-            vector.y *= factor
-
-        return vector
-
-    def find_path_through_waypoints(self, start: Point, target: Point) -> List[Point]:
-        """
-        Find a route from `start` to `target` using BFS only between the waypoints,
-        ensuring that paths between neighbors are clear.
-        """
-        start_pos = (start.x, start.y)
-        target_pos = (target.x, target.y)
-        
-        queue = [(start_pos, [start_pos])]
-        visited = set()
-        visited.add(start_pos)
-
-        waypoint_positions = {(wp.x, wp.y): wp for wp in self.waypoints}
-
-        while queue:
-            current_pos, path = queue.pop(0)
-
-            # Check if we are currentrly in the target
-            if current_pos == target_pos:
-                return [waypoint_positions[pos] for pos in path]
-
-            current_wp = waypoint_positions[current_pos]
-            neighbors = self.find_neighbors(self.waypoints, current_wp)
-
-            # Check posible paths with the neighbors
-            for neighbor in neighbors:
-                neighbor_pos = (neighbor.x, neighbor.y)
-                if neighbor_pos not in visited and self.is_path_clear(current_pos, neighbor_pos):
+                if neighbor_pos not in visited and self.is_path_clear(
+                    current_pos, neighbor_pos
+                ):
                     visited.add(neighbor_pos)
                     queue.append((neighbor_pos, path + [neighbor_pos]))
 
@@ -225,34 +188,34 @@ class Nav2PointRuleNode(RuleNode):
         self.point.x = rospy.get_param("~point_x", self.point.x)
         self.point.y = rospy.get_param("~point_y", self.point.y)
 
-        if (self.point.x != self.prev_point.x or self.point.y != self.prev_point.y):
-
+        if self.point.x != self.prev_point.x or self.point.y != self.prev_point.y:
             # Get the closer waypoint to the position of the robots
             start = Point()
             for robot in self.robots:
                 start.x += robot.pose.pose.position.x
                 start.y += robot.pose.pose.position.y
-            
+
             start.x = start.x / self.n_robots
             start.y = start.y / self.n_robots
 
             dist = None
             closer_2_start = Point()
             for wp in self.waypoints:
-                if (dist == None or calc_distance(start, wp) < dist):
+                if dist is None or calc_distance(start, wp) < dist:
                     dist = calc_distance(start, wp)
                     closer_2_start = wp
             nav2point_vectors = VectorArray()
-            
+
             dist = None
             closer_2_target = Point()
             for wp in self.waypoints:
-            
-                if (dist == None or calc_distance(self.point, wp) < dist):
+                if dist is None or calc_distance(self.point, wp) < dist:
                     dist = calc_distance(self.point, wp)
                     closer_2_target = wp
 
-            self.path = self.find_path_through_waypoints(closer_2_start, closer_2_target)
+            self.path = self.find_path_through_waypoints(
+                closer_2_start, closer_2_target
+            )
 
             self.path.append(self.point)
             if self.path:
@@ -265,20 +228,20 @@ class Nav2PointRuleNode(RuleNode):
         for robot in self.robots:
             average_position.x += robot.pose.pose.position.x
             average_position.y += robot.pose.pose.position.y
-        
+
         average_position.x = average_position.x / self.n_robots
         average_position.y = average_position.y / self.n_robots
 
         # Delete the waypoint from the path if the swarm has arrived
-        if (calc_distance(average_position, self.path[0]) < self.distance_threshold):
-            if (self.path[0] != self.point):
+        if calc_distance(average_position, self.path[0]) < self.distance_threshold:
+            if self.path[0] != self.point:
                 self.path.pop(0)
         nav2point_vectors = VectorArray()
-        
+
         dist = None
         closer_2_target = Point()
         for wp in self.waypoints:
-            if (dist == None or calc_distance(self.point, wp) < dist):
+            if dist is None or calc_distance(self.point, wp) < dist:
                 dist = calc_distance(closer_2_target, wp)
                 closer_2_target = wp
         nav2point_vectors = VectorArray()
@@ -291,6 +254,7 @@ class Nav2PointRuleNode(RuleNode):
         self.prev_point.y = self.point.y
 
         self.pub.publish(nav2point_vectors)
+
 
 if __name__ == "__main__":
     rospy.init_node("nav2point_rule")
